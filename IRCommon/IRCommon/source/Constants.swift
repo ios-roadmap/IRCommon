c17216ace0eb03cdf77c18977ec57114abef8faf
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2025-05-25 22:37:12 +0300

    common

---
+++ IRCommon/IRCommon/source/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  IRCommon
+//
+//  Created by Ömer Faruk Öztürk on 25.05.2025.
+//
+
+import Foundation
+
+public enum Constants {
+    public static let randomImage = "https://picsum.photos/600/600"
+}
